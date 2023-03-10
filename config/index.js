79588272c60e5ca4a3e77f17b0e78c1395374af8
goduer <526140067@qq.com>
2023-03-10 20:10:18 +0800

    fix: 配置修改

--- config/index.js
+++ config/index.js
@@ -47,7 +47,7 @@ const config = {
     prebundle: { enable: false }
   }, 
   cache: {
-    enable: false // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
+    enable: true // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
   },
   sass:{
     resource: [
