4430925758dcb91f855bd76bc72fc020ade33bb6
goduer <526140067@qq.com>
2023-03-14 20:53:02 +0800

    feat: 导入wx sdk包

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,27 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import wx from 'weixin-js-sdk-ts'
 
 import './app.scss'
 
 const App = createApp({
   onShow (options) {
-    console.log(options)
+    console.log(options, wx)
+    // const timestamp: number = new Date().getTime()
+    // wx.config({
+    //   debug: true,
+    //   appId: 'wx0d22ad52fb6544db',
+    //   timestamp: timestamp,
+    //   nonceStr: '', 
+    //   signature: '',
+    //   jsApiList: []
+    // })
+    wx.ready(function(){
+      
+    })
+    wx.error(function(res){
+      console.log(res)
+    })
   },
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
 })
---
+++ src/pages/user/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <view class="user">
+    users
+  </view>
+</template>
+
+<script lang="ts">
+  export default {
+
+  }
+</script>
+<style lang="scss" scoped>
+
+</style>
+
+
